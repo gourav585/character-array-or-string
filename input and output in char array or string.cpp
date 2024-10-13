5d42fa4b2bcf09c462b7ca2ef6f034095602ca58
Gourav Rajak <148696915+gourav585@users.noreply.github.com>
2024-10-13 11:46:19 +0530

    Add files via upload

---
+++ input and output in char array or string.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch[8] ={'g','o','u','r','a','v','\0','r'};
+    for(int i=0;i<6;i++){
+       cout<<ch[i];
+       
+    }
+    cout<<endl;
+    char str[] = "work";
+    cout<<str;
+    
+    cout<<endl;
+    
+     char gk[] = {'a','b','c','d'};
+    cout<<gk;
+    
+    cout<<endl;
+    
+     char string[] = "gourav rajak";
+    cout<<string;
+    
+}
