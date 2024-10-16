a7263d63c4c7ae893d5646657dbebfeef94eb989
Gourav Rajak <148696915+gourav585@users.noreply.github.com>
2024-10-16 20:09:34 +0530

    Add files via upload

---
+++ how to add two strings.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+	string s1 = "fam";
+	string s2 = "ily";
+//	s1.append(s2);  // append is used to add two strings
+//	cout<<s1<<endl;// this is frist way to add 
+	cout<<s1+s2<<endl; //ans this is second way to add stringd 
+	
+	
+	cout<<s1[1]<<endl;
+}
